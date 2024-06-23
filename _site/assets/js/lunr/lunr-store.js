7b780b15182b8037c164b7e352e0da7476733d48
half <half@localhost.localdomain>
2024-06-23 18:51:08 -0400

    tester post

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "first - post",
+        "excerpt":"Testing   header  ","categories": ["testing"],
+        "tags": ["testing"],
+        "url": "/testing/test/",
+        "teaser": "/assets/images/logo.png"
+      }]
