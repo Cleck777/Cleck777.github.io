e05081be4675094cef98aa9eeaaf75f53fa3852b
half <half@localhost.localdomain>
2024-06-23 19:20:02 -0400

    ras2

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Reverse agent with Rasberry Pie",
         "excerpt":"Testing   header  ","categories": ["pentesting, tools"],
         "tags": ["pentesting, ethical hacking, tools"],
-        "url": "/pentesting,%20tools/test/",
+        "url": "/pentesting,%20tools/rasberrypi/",
         "teaser": "/assets/images/evilpi.png"
       }]
