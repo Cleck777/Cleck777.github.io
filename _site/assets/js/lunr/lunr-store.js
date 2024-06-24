932e65bc533eb660692360fec88ffd6f506c23cc
half <half@localhost.localdomain>
2024-06-23 22:32:05 -0400

    update5

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": null,
+        "excerpt":"Background While doing some of the Pro-labs for HTB I recently discovered ligolo-ng Ligolo-ng is a simple, lightweight and fast tool that allows pentesters to establish tunnels from a reverse TCP/TLS connection using a tun interface (without the need of SOCKS). The program is really cool and easy to use....","categories": [],
+        "tags": null,
+        "url": "/2024-06-23-EvilPI/",
+        "teaser": null
+      }]
