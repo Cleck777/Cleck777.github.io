51444cb3399157de4693d80cb5a404328f977054
half <half@localhost.localdomain>
2024-06-25 13:37:16 -0400

    2024-06-24-update

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "EvilPI: Using a Raspberry Pi as an Agent into an Internal Network",
+        "excerpt":"Background While doing some of the Pro-labs for HTB, I recently discovered ligolo-ng. Ligolo-ng is a simple, lightweight, and fast tool that allows pentesters to establish tunnels from a reverse TCP/TLS connection using a tun interface (without the need for SOCKS). The program is really cool and easy to use....","categories": ["pentesting"],
+        "tags": ["pentesting","ethical hacking","tools"],
+        "url": "/pentesting/EvilPI/",
+        "teaser": "/assets/images/evilpi.png"
+      }]
